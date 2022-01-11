4e633c7c9518a0bdcc21a3717932af0e066d9ba0
DESKTOP-JNNSRFF\admin <2560902979@qq.com>
2022-01-11 09:57:10 +0800

    feat(git): add commitizen

--- .cz-config.js
+++ .cz-config.js
@@ -1,44 +1,52 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-11 09:30:59
+ * @LastEditTime: 2022-01-11 09:50:07
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-admin\.cz-config.js
+ */
 module.exports = {
   types: [{
-      value: 'feat',
-      name: 'feat:     新功能'
-    },
-    {
-      value: 'fix',
-      name: 'fix:      修复'
-    },
-    {
-      value: 'docs',
-      name: 'docs:     文档变更'
-    },
-    {
-      value: 'style',
-      name: 'style:    代码格式(不影响代码运行的变动)'
-    },
-    {
-      value: 'refactor',
-      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
-    },
-    {
-      value: 'perf',
-      name: 'perf:     性能优化'
-    },
-    {
-      value: 'test',
-      name: 'test:     增加测试'
-    },
-    {
-      value: 'chore',
-      name: 'chore:    构建过程或辅助工具的变动'
-    },
-    {
-      value: 'revert',
-      name: 'revert:   回退'
-    },
-    {
-      value: 'build',
-      name: 'build:    打包'
-    }
+    value: 'feat',
+    name: 'feat:     新功能'
+  },
+  {
+    value: 'fix',
+    name: 'fix:      修复'
+  },
+  {
+    value: 'docs',
+    name: 'docs:     文档变更'
+  },
+  {
+    value: 'style',
+    name: 'style:    代码格式(不影响代码运行的变动)'
+  },
+  {
+    value: 'refactor',
+    name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+  },
+  {
+    value: 'perf',
+    name: 'perf:     性能优化'
+  },
+  {
+    value: 'test',
+    name: 'test:     增加测试'
+  },
+  {
+    value: 'chore',
+    name: 'chore:    构建过程或辅助工具的变动'
+  },
+  {
+    value: 'revert',
+    name: 'revert:   回退'
+  },
+  {
+    value: 'build',
+    name: 'build:    打包'
+  }
   ],
   // override the messages, defaults are as follows
   messages: {
@@ -50,7 +58,7 @@ module.exports = {
     body: '请输入详细描述(可选，待优化去除，跳过即可):',
     // breaking: 'List any BREAKING CHANGES (optional):\n',
     footer: '请输入要关闭的issue(待优化去除，跳过即可):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
+    confirmCommit: '确认使用以上信息提交？(y/n)'
   },
   allowCustomScopes: true,
   // allowBreakingChanges: ['feat', 'fix'],
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-01-11 09:30:59
+ * @LastEditTime: 2022-01-11 09:48:59
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-admin\src\views\Home.vue
+-->
 
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
